b0e719151f6c67d0313e007638b31d8126faea0d
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-05-18 11:52:40 -0500

    Ejercicio 01

---
+++ src/tema_01/Ejercicio_01.java
@@ -0,0 +1,18 @@
+package tema_01;
+
+public class Ejercicio_01 {
+    public static void main(String[] args) {
+        int a = 500000000;
+        long b = 800000000;
+        double c=3.24;
+        boolean d = false;
+        String s = "Hola Mundo";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+
+    }
+}
