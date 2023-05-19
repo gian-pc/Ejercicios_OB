76a66c3b02b1e632d739c450545a7dc75c21a0aa
Gian Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2023-05-18 22:08:30 -0500

    Ejercicio 03

---
+++ src/tema_03/Ejercicio_03.java
@@ -0,0 +1,14 @@
+package tema_03;
+
+public class Ejercicio_03 {
+    public static void main(String[] args) {
+        String[] nombres = {"Gian", "Carlos", "Paucar", "Cortez"};
+
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.print(nombres[i] + " ");
+        }
+
+
+    }
+}
